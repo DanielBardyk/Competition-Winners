e55d068cb121af2a8ce314827cb148fe2d2f6e9e
DanielBardyk <github.daniel31@gmail.com>
2023-05-14 17:34:17 +0200

    Implemented function to get list of countries from a file

--- functions.cpp
+++ functions.cpp
@@ -24,3 +24,43 @@ std::vector<fs::path> readDir(const fs::path& dpath)
 
 bool isCSVFile(const fs::path& p)
 { return fs::is_regular_file(p) && p.extension() == CSV_EXTENSION; }
+
+std::vector<Country> getCountries(const std::vector<fs::path>& paths)
+{
+	std::vector<Country> countries;
+	std::vector<int> countryVotes;
+	std::string line, rowElem, countryName;
+	std::ifstream inputFile;
+	
+	for (const fs::path & filePath : paths)
+	{
+		inputFile.open(filePath);
+
+		if(inputFile.is_open())
+		{
+			std::getline(inputFile, line);
+
+			while(std::getline(inputFile, line))
+			{
+				std::stringstream str(line);
+
+				std::getline(str, rowElem, ',');
+				countryName = rowElem;
+
+				Country country(countryName);
+
+				while(std::getline(str, rowElem, ','))
+					country.addVote(stoi(rowElem));
+
+				std::getline(str, rowElem);
+				country.addVote(stoi(rowElem));
+
+				countries.push_back(country);
+			}
+
+			inputFile.close();
+		} else 
+			throw std::runtime_error("Cannot open " + filePath.filename().string());
+	}
+	return countries;
+}
--- functions.h
+++ functions.h
@@ -5,6 +5,8 @@
 #pragma once
 #include <vector>
 
+#include "Country.h"
+
 #define CSV_EXTENSION ".csv"
 #define WINNERS_NUM 10
 
@@ -15,3 +17,5 @@ bool isDirExists(const fs::path&);
 std::vector<fs::path> readDir(const fs::path&);
 
 bool isCSVFile(const fs::path&);
+
+std::vector<Country> getCountries(const std::vector<fs::path>& paths);
